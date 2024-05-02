cc82d3a7f2c1150756f43eef7300ee3ada8dc412
Rffcod <110072804+Rffcod@users.noreply.github.com>
2024-05-02 13:39:11 +0700

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>Quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> Login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact</h3>
+         <a href="tel:+6285732917536"><i class="fas fa-phone"></i> +62 857 329 175 36 </a>
+         <a href="tel:9900000000"><i class="fas fa-phone"></i> +62 897 001 848 3 </a>
+         <a href="mailto:arifmalangg@gmail.com"><i class="fas fa-envelope"></i> arifmalangg@gmail.com </a>
+         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> Malang, Indonesia </a>
+      </div>
+
+      <div class="box">
+         <h3>Follow</h3>
+         <a href="https://www.facebook.com/Mas Hadi" target="_blank"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="https://tiktok.com/cepirit.com" target="_blank"><i class="fab fa-tiktok"></i>Tiktok</a>
+         <a href="https://www.instagram.com/malanggarif/" target="_blank"><i class="fab fa-instagram"></i>Instagram</a>
+         <a href="https://www.linkedin.com/" target="_blank"><i class="fab fa-linkedin"></i>Linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>BRODIN</span> | all rights reserved!</div>
+
+</footer>
